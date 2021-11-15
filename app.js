ce80ef9c7a5052ecbe5b84c9b805c88991013db0
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-15 16:32:02 +0000

    Set up posts page to view individual posts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,8 +17,9 @@ app.use(express.static("public"));
 let allPosts = [];
 
 app.get('/', (req, res) => {
-    res.render('home', { homeContent: homeContent });
-    console.log(allPosts);
+    res.render('home', { 
+        homeContent: homeContent,
+        allPosts: allPosts });
 });
 
 app.get('/about', (req, res) => {
@@ -37,8 +39,24 @@ app.post('/compose', (req, res) => {
         title: req.body.postTitle,
         body: req.body.postBody
     };
+
     allPosts.push(newPost);
+
     res.redirect('/');
+});
+
+app.get('/posts/:postName', (req, res) => {
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+    allPosts.forEach((post) => {
+        const storedTitle = _.lowerCase(post.title);
+        if(storedTitle === requestedTitle) {
+            res.render('post', {
+                title: post.title,
+                body: post.body
+            });
+        }
+    })
 });
 
 
